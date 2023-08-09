a64f94f0ee46c14975e8ab4310a1dec9fab98eca
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-08-09 19:42:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Q1.1
+// alert('Hello dear client')
+
+// Q1.2
+
+// alert('Error! please enter a valid password');
+
+// Q1.3
+
+// alert('Welcome to JS Land..... \nHappy coding');
+
+// Q1.4
+
+alert('Welcome to JS Land.....');
+alert('Happy coding');
+

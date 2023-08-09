8b7dea5800da7a9111e6996ab6b2174c243a576f
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-08-09 22:02:22 +0500

    Q#2 all parts done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 
 // Q1.5
 
-alert("Hello...i can run it through browser's console");
+// alert("Hello...i can run it through browser's console");
 
 // Q1.6 
 // Done
@@ -25,6 +25,95 @@ alert("Hello...i can run it through browser's console");
 
 // Q1.7
 // Done in html file
+
+
+// Q2.1
+
+// var userName;
+
+// Q2.2
+
+// var myName= "Hafiz Noor Ul Huda";
+// alert(myName)
+
+// Q2.3
+
+// var message;
+// message="Hello World";
+// alert(message);
+
+
+// Q2.4
+
+
+// var name1= "Ali";
+// var age= "15 years old"
+// var subject= "Cartified web and app development";
+
+// alert(name1)
+// alert(age)
+// alert(subject)
+
+
+// Q2.5
+
+// var menu= "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(menu)
+
+// Q2.6
+
+// var myEmail="example@example.com";
+// alert("My email address is "+ myEmail)
+
+// Q2.7
+
+// var book="A smarter way to learn JS";
+// alert("I am trying to learn the book called "+ book)
+
+// Q2.8
+
+// document.write("Yah! I can write HTML content through JS")
+
+
+// Q2.9
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
